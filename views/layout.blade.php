ab64615a626be0041bf45dc025ecb2c155922cf0
Kolya1222 <48295740+Kolya1222@users.noreply.github.com>
2025-07-28 16:30:21 +0700

    Update layout.blade.php

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -1,10 +1,10 @@
-<link rel="stylesheet" href="{{ MODX_BASE_URL }}assets/modules/Formbuild/css/bootstrap.min.css">
-<link rel="stylesheet" href="{{ MODX_BASE_URL }}assets/modules/Formbuild/css/bootstrap-icons.css">
-<link rel="stylesheet" href="{{ MODX_BASE_URL }}assets/modules/Formbuild/css/style.css">
+<link rel="stylesheet" href="{{ MODX_BASE_URL }}assets/modules/formbuild/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ MODX_BASE_URL }}assets/modules/formbuild/css/bootstrap-icons.css">
+<link rel="stylesheet" href="{{ MODX_BASE_URL }}assets/modules/formbuild/css/style.css">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 @yield('buttons')
 <div class="sectionBody">
     @yield('body')
 </div>
-<script src="{{ MODX_BASE_URL }}assets/modules/Formbuild/js/bootstrap.bundle.min.js"></script>
-<script type="module" src="{{ MODX_BASE_URL }}assets/modules/Formbuild/js/main.js"></script>
+<script src="{{ MODX_BASE_URL }}assets/modules/formbuild/js/bootstrap.bundle.min.js"></script>
+<script type="module" src="{{ MODX_BASE_URL }}assets/modules/formbuild/js/main.js"></script>
